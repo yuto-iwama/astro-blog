9fd7a08efbe38da15996371ba1c0161b37f4a3ad
yutoiwama <yutoiwama@gmail.com>
2024-02-08 09:03:19 +0900

    tailwindをインストール
    https://docs.astro.build/en/guides/integrations-guide/tailwind/

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://fantastic-macaron-c73631.netlify.app/",
+  integrations: [tailwind()],
 });
