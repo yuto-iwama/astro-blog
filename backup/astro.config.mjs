f5781039fec136d2a51df2140ef2f65914ce270f
yutoiwama <yutoiwama@gmail.com>
2024-02-10 14:31:47 +0900

    backup削除

--- backup/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "astro/config";
-
-import preact from "@astrojs/preact";
-
-// https://astro.build/config
-export default defineConfig({
-  site: "https://fantastic-macaron-c73631.netlify.app/",
-  integrations: [preact()]
-});
